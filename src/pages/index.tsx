355e1fd3b62d8be395526fda43aac71ece6ff8e1
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-08-28 17:17:59 +0900

    update

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Button>This is Button</Button>
+    </div>
+  );
+};
+
+export default Home;
