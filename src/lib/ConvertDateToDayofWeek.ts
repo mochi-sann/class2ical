8b7e0c3f7d42c56bcfb11980f88c451f39620ccb
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-09-02 19:21:38 +0900

    変換するのをアップデート

--- src/lib/ConvertDateToDayofWeek.ts
+++ src/lib/ConvertDateToDayofWeek.ts
@@ -5,9 +5,9 @@ export type ConvertDateToDayofWeekReturnType = number;
 /*
  * 日付を入力したら曜日を返す
  * 返す曜日は数字
- * 例 月曜日 -> 0
- * 例 火曜日 -> 1
- *  日曜日 -> 6
+ * 例 日曜日 -> 0
+ * 例 月曜日 -> 1
+ *  土曜日 -> 6
  * **/
 const ConvertDateToDayofWeek = (
   Args: ConvertDateToDayofWeekArgsType
--- src/lib/test/ConvertDateToDayOfWeek.test.ts
+++ src/lib/test/ConvertDateToDayOfWeek.test.ts
@@ -7,3 +7,7 @@ test("Convert to Icaldeta friday", () => {
   const day = dayjsWapper("2022-9-2").format("YYYY-MM-DD");
   expect(ConvertDateToDayofWeek(day)).toBe(5);
 });
+test("Convert to Icaldeta Monday", () => {
+  const day = dayjsWapper("2022-9-4").format("YYYY-MM-DD");
+  expect(ConvertDateToDayofWeek(day)).toBe(0);
+});
