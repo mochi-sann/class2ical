bed1fb86581e3aca7619434992dfbd04684b10f4
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-09-04 10:27:44 +0900

    テストを追加

--- src/lib/test/FormDateToIcal.test.ts
+++ src/lib/test/FormDateToIcal.test.ts
@@ -58,3 +58,53 @@ test("Convert to Icaldeta 曜日と startDateが違う", () => {
   };
   expect(FormDateToIcal(Value)).toStrictEqual(TestResultValue);
 });
+test("Convert to Icaldeta:3", () => {
+  const Value: FormDateToIcalArgsType = {
+    startDate: "2022-09-05",
+    count: 8,
+    periodNumber: 5,
+    dayOfweek: "Fri",
+    summary: "授業の名前",
+    description: "授業の説明とかを書くぞ",
+    url: "https://google.com/",
+  };
+
+  const TestResultValue: FormDateToIcalReturnType = {
+    summary: "授業の名前",
+    description: "授業の説明とかを書くぞ",
+    start: dayjsWapper("2022-09-09 17:40:00+09:00").toDate(),
+    url: "https://google.com/",
+    end: dayjsWapper("2022-09-09 17:40:00+09:00").add(90, "minutes").toDate(),
+    timezone: "Asia/Tokyo",
+    repeating: {
+      count: 8,
+      freq: ICalEventRepeatingFreq["WEEKLY"],
+    },
+  };
+  expect(FormDateToIcal(Value)).toStrictEqual(TestResultValue);
+});
+test("Convert to Icaldeta:4", () => {
+  const Value: FormDateToIcalArgsType = {
+    startDate: "2022-09-30",
+    count: 8,
+    periodNumber: 5,
+    dayOfweek: "Mon",
+    summary: "授業の名前",
+    description: "授業の説明とかを書くぞ",
+    url: "https://google.com/",
+  };
+
+  const TestResultValue: FormDateToIcalReturnType = {
+    summary: "授業の名前",
+    description: "授業の説明とかを書くぞ",
+    start: dayjsWapper("2022-10-03 17:40:00+09:00").toDate(),
+    url: "https://google.com/",
+    end: dayjsWapper("2022-10-03 17:40:00+09:00").add(90, "minutes").toDate(),
+    timezone: "Asia/Tokyo",
+    repeating: {
+      count: 8,
+      freq: ICalEventRepeatingFreq["WEEKLY"],
+    },
+  };
+  expect(FormDateToIcal(Value)).toStrictEqual(TestResultValue);
+});
