017bdb8df09717270173f02220ec8b197f28cb2d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-12-12 21:19:39 +0900

    ファイルの保存のコードをファイル分割

--- src/hooks/useClassTableIcal.tsx
+++ src/hooks/useClassTableIcal.tsx
@@ -2,6 +2,8 @@ import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import ical, { ICalEvent, ICalEventData } from "ical-generator";
 
+import { DownloadBrowsser } from "src/lib/DownloadBrowser";
+
 export type useClassTableIcalProps = {
   init: Array<ICalEvent | ICalEventData>;
 };
@@ -28,29 +30,7 @@ export const useClassTableIcal = (
   // ファイルをダウンロード
   const DownloadFile = (event: useClassTableIcalProps["init"]): string => {
     try {
-      const calendar = ical({ name: "授業の時間" });
-
-      calendar.description("大学の授業のカレンダー");
-
-      calendar.clear();
-      event.map((event) => {
-        calendar.createEvent(event);
-      });
-      const CalenderBlob = calendar.toBlob() || "";
-      const CalenderUrl = calendar.toURL();
-      let dummy_a_el = document.createElement("a");
-
-      document.body.appendChild(dummy_a_el);
-
-      // a 要素の href 属性に Object URL をセット
-      dummy_a_el.href = CalenderUrl;
-
-      // a 要素の download 属性にファイル名をセット
-      dummy_a_el.download = "大学の授業.ics";
-      dummy_a_el.click();
-      // document.body.removeChild(dummy_a_el);
-
-      return CalenderUrl || "/";
+      return DownloadBrowsser(event);
     } catch (error) {
       console.error(" can not downloadl", error);
       return "/";
