43fe8ed1b4df8d1ad0a161266762ef0ca71279b1
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-01-12 12:37:20 +0900

    vitestのテストで e2e テスト用のファイルが走らないようにした

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    exclude: [...configDefaults.exclude, "**/e2e/**"],
   },
 });
